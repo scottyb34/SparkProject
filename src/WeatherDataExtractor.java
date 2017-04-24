6cfda57364b5dd478f66d0198b98ce759eb06d10
eschrae <eshawk23@gmail.com>
2017-04-23 19:28:55 -0600

    updated WBANs and filtering for malformed data

--- src/WeatherDataExtractor.java
+++ src/WeatherDataExtractor.java
@@ -1,9 +1,7 @@
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -12,20 +10,19 @@ import org.apache.commons.io.FileUtils;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function;
 
 public class WeatherDataExtractor {
 	public static Map<String, String> wbanConversion;
 
 	public static void main(String[] args) throws Exception {
 		wbanConversion = new HashMap<String, String>();
-		wbanConversion.put("13881", "Charlotte");
-		wbanConversion.put("94870", "Urbana-Champaign");
-		wbanConversion.put("03149", "Phoenix");
-		wbanConversion.put("23054", "Las Vegas");
-		wbanConversion.put("94811", "Madison");
-		wbanConversion.put("04805", "Cleveland");
-		wbanConversion.put("94823", "Pittsburgh");
+		wbanConversion.put("13881", "NC");
+		wbanConversion.put("94870", "IL");
+		wbanConversion.put("23183", "AZ");
+		wbanConversion.put("23169", "NV");
+		wbanConversion.put("14837", "WI");
+		wbanConversion.put("14820", "OH");
+		wbanConversion.put("94823", "PA");
 
 		System.out.println(System.getProperty("hadoop.home.dir"));
 
@@ -46,37 +43,19 @@ public class WeatherDataExtractor {
 
 		// List<Integer> ind =(List<Integer>) Arrays.asList(new
 		// Integer[]{6,10,28,30,40});
+		Integer[] first = new Integer[] { 0, 1, 4, 6, 13, 14, 19 };
 
-		JavaRDD<String> wban = rdd.filter(e -> e.substring(0, 5).matches("13881|94870|03149|23054|94811|04805|94823"))
+		Integer[] second = new Integer[] { 0, 1, 6, 10, 28, 30, 40 };
+
+		JavaRDD<String> wban = rdd.filter(e -> e.substring(0, 5).matches("13881|94870|23183|23169|14837|14820|94823"))
 				.map(e -> (String) e.replaceFirst("^.....", wbanConversion.get(e.substring(0, 5))))
 				.map(e -> Arrays.asList(e.split(",")))
 				.map(e -> IntStream.range(0, e.size())
-						.filter(i -> Arrays.asList(new Integer[] { 0, 6, 10, 28, 30, 40 }).contains(new Integer(i)))
-						.mapToObj(e::get).map(e2 -> e2.toString()).collect(Collectors.joining(",")));
+						.filter(i -> Arrays.asList(e.size() > 30 ? second : first).contains(new Integer(i)))
+						.mapToObj(e::get).map(e2 -> e2.toString())
+						.collect(Collectors.joining(",")));
 
-		// JavaPairRDD<String, Integer> counts = rdd.flatMap(x ->
-		// Arrays.asList(x.split(" ")).iterator())
-		// .mapToPair(x -> new Tuple2<String, Integer>(x, 1)).reduceByKey((x, y)
-		// -> x + y);
-		//
-		// List<Tuple2<String, Integer>> finalCounts = counts.filter((x) ->
-		// x._1().contains("@")).collect();
-		//
-		// for (Tuple2<String, Integer> count : finalCounts)
-		// System.out.println(count._1() + " " + count._2());
-		//
-		// counts = rdd.flatMap(x -> Arrays.asList(x.split(" ")).iterator())
-		// .mapToPair(x -> new Tuple2<String, Integer>(x, 1)).reduceByKey((x, y)
-		// -> x + y);
-		//
-		// counts = counts.filter((x) -> x._2() > 20);
-		//
-		// long time = System.currentTimeMillis();
-		// long countEntries = counts.count();
-		// System.out.println(countEntries + ": " +
-		// String.valueOf(System.currentTimeMillis() - time));
-
-		// counts.saveAsTextFile(outputPath);
+	
 		wban.saveAsTextFile(outputPath);
 		sc.close();
 
